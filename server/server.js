4fd53ce8776a626cb3ff7ff4ccdf54bddeec44d6
Moshe Deitsch <me@moshedeitsch.com>
2020-03-25 17:40:12 +0200

    change server

--- server/server.js
+++ server/server.js
@@ -3,14 +3,25 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-
 app.use(cors());
+app.use(express.json());
+
+
+let messeges = ["hello", "hi","Vue Love you"];
+
 app.get('/messeges', (req,res) => {
     
-    const messeges = ["hello", "hi","Vue Love you"];
 
     res.send(messeges);
 
 });
+
+
+
+app.post('/messeges', (req,res) => {
+    let msg = req.body;
+    console.log(msg);
+    messeges.push(msg.message);
+  })
 
 app.listen(port, () => console.log(`server is working ${port}`));
